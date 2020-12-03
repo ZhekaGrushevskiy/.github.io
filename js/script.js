f5224e73b55afd342011d602f5c2d95ad835460e
ZhekaGrushevskiy <75371987+ZhekaGrushevskiy@users.noreply.github.com>
2020-12-04 00:09:17 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,15 @@ $(document).ready(function(){
     $('.open_menu_mobile').click(function(event){
         $('.open_menu_mobile,.menu_mobile').toggleClass('active');
     });
+    $('#arrow-right').click(function(event){
+        $('.onlrec').toggleClass('active');
+    }); 
+    var $arrow_nav = $('.arrow_nav');
+    $(window).on("scroll",function(){
+        if ($(window).scrollTop() >=600){
+            $arrow_nav.addClass('active');
+        }else{
+            $arrow_nav.removeClass('active');
+        }
+    });
 });
