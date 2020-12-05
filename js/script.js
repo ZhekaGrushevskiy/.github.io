cd931267de1dc2233dc9bc7bee8ea8e48e9c4728
ZhekaGrushevskiy <75371987+ZhekaGrushevskiy@users.noreply.github.com>
2020-12-06 01:49:22 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.open_menu_mobile,.exit').click(function(event){
         $('.open_menu_mobile,.menu_mobile').toggleClass('active');
     });
-    $('a[href="#onlinerecord"]').click(function(event){
+    $('.openexit').click(function(event){
         $('.exit').addClass('active');
     });
     $('.exit').click(function(event){
@@ -13,10 +13,22 @@ $(document).ready(function(){
     }); 
     var $arrow_nav = $('.arrow_nav');
     $(window).on("scroll",function(){
-        if ($(window).scrollTop() >=600){
+        if ($(window).scrollTop() > 85){
             $arrow_nav.addClass('active');
         }else{
             $arrow_nav.removeClass('active');
+        }     
+    });
+    var lastScrollTop = 0;
+    var $header = $('#header');
+    $(window).scroll(function(event){
+        var st = $(this).scrollTop();
+        if(st>lastScrollTop){
+            $header.addClass('active');
+        }else{
+            $header.removeClass('active');
         }
+        lastScrollTop = st;
     });
 });
+
